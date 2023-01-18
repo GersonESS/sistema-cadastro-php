b8c03a6a3280c432376303bb7bb5fe3c99422ed5
Gerson Santos <62805855+GersonESS@users.noreply.github.com>
2023-01-18 20:09:54 -0300

    Update edit - Copia.php

--- edit - Copia.php
+++ edit - Copia.php
@@ -1,10 +1,11 @@
 <?php
 include_once('config.php');
+
    if(!empty($_GET['id']))
    {
         $id = $_GET['id'];
-        $sqlselect = "SELECT * FROM usuarios WHERE id=$id"; 
-        $result = $conexao->query($sqlselect);
+        $sqlSelect = "SELECT * FROM usuarios WHERE id=$id"; 
+        $result = $conexao->query($sqlSelect);
     if($result->num_rows > 0)
     {
         while($user_data = mysqli_fetch_assoc($result))
@@ -124,22 +125,22 @@ include_once('config.php');
                 <legend><b>Usuarios</b></legend>
                 <br>
                 <div class="inputBox">
-                    <input type="text" name="nome" id="nome" class="inputUser" value=<?php echo $nome ?> required>
-                    <label for="none" class="labelInput">Nome Completo</label>
+                    <input type="text" name="nome" id="nome" class="inputUser" value=<?php echo $nome; ?> required>
+                    <label for="nome" class="labelInput">Nome Completo</label>
                 </div>   
                 <br><br>
                 <div class="inputBox">
-                    <input type="password" name="senha" id="senha" class="inputUser" value=<?php echo $senha ?> required>
+                    <input type="password" name="senha" id="senha" class="inputUser" value=<?php echo $senha; ?> required>
                     <label for="senha" class="labelInput">Senha</label>
                 </div>
                 <br><br>
                 <div class="inputBox">
-                    <input type="email" name="email" id="email" class="inputUser" value=<?php echo $email ?> required>
+                    <input type="email" name="email" id="email" class="inputUser" value=<?php echo $email; ?> required>
                     <label for="email" class="labelInput">Email</label>
                 </div>
                 <br><br>
                 <div class="inputBox">
-                    <input type="tel" name="telefone" id="telefone" class="inputUser" value=<?php echo $telefone ?> required>
+                    <input type="tel" name="telefone" id="telefone" class="inputUser" value=<?php echo $telefone; ?> required>
                     <label for="telefone" class="labelInput">Telefone</label>
                 </div>
                 <p>Sexo</p>
@@ -153,20 +154,20 @@ include_once('config.php');
                 <label for="outro">Outro</label>
                 <br><br>
                 <label for="data_nascimento">Data de Nascimento</label>
-                <input type="date" name="data_nascimento" id="data_nascimento"  value="<?php echo $data_nasc ?>" required>              
+                <input type="date" name="data_nascimento" id="data_nascimento"  value="<?php echo $data_nasc; ?>" required>              
                 <br><br>
                 <div class="inputBox">
-                    <input type="text" name="cidade" id="cidade" class="inputUser"  value="<?php echo $cidade ?>" required>
+                    <input type="text" name="cidade" id="cidade" class="inputUser"  value="<?php echo $cidade; ?>" required>
                     <label for="cidade" class="labelInput">Cidade</label>
                 </div>
                 <br><br>
                 <div class="inputBox">
-                    <input type="text" name="estado" id="estado" class="inputUser"  value="<?php echo $estado ?>" required>
+                    <input type="text" name="estado" id="estado" class="inputUser"  value="<?php echo $estado; ?>" required>
                     <label for="estado" class="labelInput">Estado</label>
                 </div>
                 <br><br>
                 <div class="inputBox">
-                    <input type="text" name="endereco" id="endereco" class="inputUser"  value="<?php echo $endereco ?>" required>
+                    <input type="text" name="endereco" id="endereco" class="inputUser"  value="<?php echo $endereco; ?>" required>
                     <label for="endereco" class="labelInput">Endereço</label>
                 </div>
                 <br><br>
