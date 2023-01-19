94d054fd8d1fe67d4ca6176db8be0ebe582261fc
Gerson Santos <62805855+GersonESS@users.noreply.github.com>
2023-01-19 20:48:55 -0300

    f

--- login - Copia.php
+++ login - Copia.php
@@ -9,23 +9,18 @@
     body{
         font-family: Arial, Helvetica, sans-serif;
         background-image: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 54, 71));
+
         }
     div{
-        background-color: rgba(0, 0, 0, 0.6);
-        position: absolute;
+        background-color: rgba 0, 0, 0, 0, 0.8;
+        position:  absolute;
         top: 50%;
         left: 50%; 
-        transform: translate(-50%,-50%);
+        transform: translate(-50%, -50%);
         padding: 80px;
         border-radius: 15px;
-        color: #fff;
+        color: white;
     }
-    input{
-        padding: 15px;
-        border: none;
-        outline: none;
-        font-size: 15px;
-    } 
     .inputSubmit{
         background-color: dodgerblue;
         border: none;
@@ -33,11 +28,18 @@
         width: 100%;
         border-radius: 10px;
         color: white;
+    }
+    input{
+        padding: 15px;
+        border: none;
+        outline: none;
         font-size: 15px;
-     }
+    }
+     
     .inputSubmit:hover{
         background-color: deepskyblue;
         cursor: pointer;
+
     }
     </style>
 </head>
@@ -46,9 +48,9 @@
     <div>
         <h1>Login</h1>
         <form action="testLogin.php" method="POST">
-            <input type="email" name="email" placeholder="Email">
+            <input type="email" name="Email" placeholder="Email">
             <br><br>
-            <input type="password" name="senha" placeholder="Senha">
+            <input type="password" name="Senha" placeholder="Senha">
             <br><br>
             <input class="inputSubmit" type="submit" name="submit" value="Enviar">
         </form>
